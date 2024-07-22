2ba3740075c33d86b0fae2c5d18409fd58a754ec
PRINCE <167236651+tprince0634@users.noreply.github.com>
2024-07-22 14:57:20 +0530

    Add files via upload

---
+++ how to create barcode code.py
@@ -0,0 +1,23 @@
+#how to create any barcode
+import qrcode
+img=qrcode.make("hii")
+img.save("aa.png")
+
+
+#how to create a calendar in python
+import calendar
+print(calendar.calendar(2023))
+
+import calendar
+year=2024
+month=11
+print(calendar. month(year, month))
+
+
+
+
+
+
+
+
+
